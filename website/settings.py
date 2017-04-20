029c24655dd708883ef8d4efb2e08663d9fee7ce
Nikola <psychosmurf.jovic@gmail.com>
2017-04-20 03:04:55 +0000

    Added files and changes required for deployment to heroku

--- website/settings.py
+++ website/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#Heroku: Updata database configuration from $DATABSE_URL.
+import dj_database_url
+db_from_env = dj_databse_url.config(conn_max_age=500)
+DATABSE['default'].update(db_from_env)
+
+# Simplifed stat file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
